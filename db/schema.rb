972fac6bfa6829e58cb7d117b0628c93c18e09df
Ava78 <chanie1981@gmail.com>
2015-12-26 19:12:17 +0100

    Création et optimisation des objets de la table en console, création des Cours + Modif ReadMe

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151226174346) do
+ActiveRecord::Schema.define(version: 20151226175503) do
 
   create_table "cours", force: :cascade do |t|
     t.string   "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20151226174346) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "slug"
   end
 
 end
